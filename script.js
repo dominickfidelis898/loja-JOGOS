762f3704a747ce98bd701eba894e646ef17877b4
DOMINICK <sadx65g@gmail.com>
2026-02-21 06:15:53 -0300

    Implement cart functionality with localStorage

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+document.getElementById("cartCount")?.innerText = cart.length;
+
+function addToCart(product) {
+  cart.push(product);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Produto adicionado ao carrinho!");
+}
