257db4182232137c973d3c651796301c9306248a
DOMINICK <sadx65g@gmail.com>
2026-02-21 08:54:29 -0300

    Add cart functionality with localStorage support

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,12 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+function addCarrinho(nome, preco){
+  cart.push({nome, preco});
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Produto adicionado ao carrinho!");
+}
+
+function comprarAgora(nome, preco){
+  localStorage.setItem("checkout", JSON.stringify({nome, preco}));
+  window.location = "pagamento.html";
+}
