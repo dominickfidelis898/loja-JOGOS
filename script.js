e7fa630a51e9523fcc81a8a47fbf822d63e5c011
DOMINICK <sadx65g@gmail.com>
2026-02-21 06:19:07 -0300

    Add cart functionality to manage products

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function addToCart(product) {
   localStorage.setItem("cart", JSON.stringify(cart));
   alert("Produto adicionado ao carrinho!");
 }
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function openCart(){
+  if(cart.length === 0){
+    alert("Carrinho vazio");
+  } else {
+    alert("Carrinho:\n" + cart.join("\n"));
+  }
+}
+
+document.getElementById("cartCount") &&
+(document.getElementById("cartCount").innerText = cart.length);
